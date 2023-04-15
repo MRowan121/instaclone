a67ef74b401ea12b866c96b0d48292581b68e21e
Matt Rowan <matt.rowan121@gmail.com>
2023-04-15 11:50:16 -0400

    Add some content to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-
+import Image from "next/image";
 
 const Header = () => {
     return (
         <header>
-            
+            <Image />
+            <form>
+                <input type="text" placeholder="Search" />
+            </form>
+            <div>
+                <p>Home Button</p>
+                <p>+ Button</p>
+                <p>User Pic</p>
+            </div>
         </header>
     )
 }
