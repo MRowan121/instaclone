bef9a764614d6eebbb2658c38b20ccb9c9d41ad6
Matt Rowan <matt.rowan121@gmail.com>
2023-04-15 15:44:42 -0400

    Add skeleton and import stories

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,9 +1,17 @@
-
+import Stories from "./Stories";
 
 const Feed = () => {
     return(
-        <>
-        </>
+        <main>
+            <section>
+                <Stories />
+                {/* posts */}
+            </section>
+            <section>
+                {/* mini profile */}
+                {/*  */}
+            </section>
+        </main>
     )
 }
 
