b4164ae652db40645c302e55f63c516c0a259d90
Matt Rowan <matt.rowan121@gmail.com>
2023-04-15 14:36:25 -0400

    Create sticky header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,35 +8,41 @@ import {
 
 const Header = () => {
     return (
-        <header className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-            <div className="cursor-pointer h-24 w-48 relative hidden lg:inline-grid">
-                <Image 
-                    src={logo} 
-                    layout="fill"
-                    className="object-contain"
-                />
-            </div>
-            <div className="cursor-pointer h-24 w-16 relative lg:hidden">
-                <Image 
-                    src={'https://raw.githubusercontent.com/ijsto/reactnextjssnippets/master/images/logo02.png'} 
-                    layout="fill"
-                    className="object-contain"
-                />
-            </div>
-            <form className="relative mt-1">
-                <div className="absolute top-1 left-1">
-                    <AiOutlineSearch className="text-2xl text-gray-500" />
+        <header className="shadow-sm border-b sticky top-0 bg-white z-30">
+            <div className="flex items-center justify-between max-w-6xl mx-4 shadow-sm xl:mx-auto">
+                <div className="cursor-pointer h-24 w-48 relative hidden lg:inline-grid">
+                    <Image 
+                        src={logo} 
+                        layout="fill"
+                        className="object-contain"
+                    />
                 </div>
-                <input type="text" placeholder="Search" className="bg-gray-50 pl-8 border-gray-500 text-lg focus:ring-black focus:border-black rounded-md border-2" />
-            </form>
-            <div className="flex space-x-5 items-center">
-                <AiFillHome className="icon hidden md:inline-flex" />
-                <AiOutlinePlusCircle className="icon" />
-                <img 
-                    src="/headshot.png"
-                    alt="user-image" 
-                    className="h-14 rounded-full"
-                />
+                <div className="cursor-pointer h-24 w-16 relative lg:hidden">
+                    <Image 
+                        src={'https://raw.githubusercontent.com/ijsto/reactnextjssnippets/master/images/logo02.png'} 
+                        layout="fill"
+                        className="object-contain"
+                    />
+                </div>
+                <form className="relative mt-1">
+                    <div className="absolute top-1 left-1">
+                        <AiOutlineSearch className="text-2xl text-gray-500" />
+                    </div>
+                    <input 
+                        type="text" 
+                        placeholder="Search" 
+                        className="bg-gray-50 pl-8 border-gray-500 text-lg focus:ring-black focus:border-black rounded-md border-2"
+                    />
+                </form>
+                <div className="flex space-x-5 items-center">
+                    <AiFillHome className="icon hidden md:inline-flex" />
+                    <AiOutlinePlusCircle className="icon" />
+                    <img 
+                        src="/headshot.png"
+                        alt="user-image" 
+                        className="icon h-14 rounded-full"
+                    />
+                </div>
             </div>
         </header>
     )
