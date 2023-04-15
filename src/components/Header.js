172f567631ea5bfffc67757d02bbab3eb0393462
Matt Rowan <matt.rowan121@gmail.com>
2023-04-15 12:10:42 -0400

    Add logo images and responsive styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
+import logo from '../../public/logo.png'
 
 const Header = () => {
     return (
-        <header>
-            <Image />
+        <header className="flex justify-between max-w-6xl">
+            <div className="cursor-pointer h-24 w-48 relative hidden lg:inline-grid">
+                <Image 
+                    src={logo} 
+                    layout="fill"
+                    className="object-contain"
+                />
+            </div>
+            <div className="cursor-pointer h-24 w-16 relative lg:hidden">
+                <Image 
+                    src={'https://raw.githubusercontent.com/ijsto/reactnextjssnippets/master/images/logo02.png'} 
+                    layout="fill"
+                    className="object-contain"
+                />
+            </div>
             <form>
                 <input type="text" placeholder="Search" />
             </form>
