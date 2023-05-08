8b9f7c9013015aea13175552fc85636b9e32e74f
Matt Rowan <matt.rowan121@gmail.com>
2023-05-08 18:52:27 -0400

    Create & style the miniprofile component

--- src/components/MiniProfile.js
+++ src/components/MiniProfile.js
@@ -2,8 +2,17 @@
 
 const MiniProfile = () => {
     return (
-        <div>
-            <h1>MiniProfile</h1>
+        <div className="flex items-center justify-between mt-14 ml-10">
+            <img 
+                src="/headshot.png"
+                alt="user-image" 
+                className="h-16 rounded-full border p-[2px]"
+            />
+            <div className="flex-1 ml-4">
+                <h2 className="font-bold">RowanTheBoat68</h2>
+                <h3 className="text-sm text-gray-400">Welcome to InstaClone!</h3>
+            </div>
+            <button className="font-semibold text-blue-400 text-sm">Sign out</button>
         </div>
     )
 }
