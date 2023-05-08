f7cd1be2388c4930c9b82e4bbd8e862c86bfb27d
Matt Rowan <matt.rowan121@gmail.com>
2023-05-08 18:37:26 -0400

    Add responsive styling

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,8 +3,8 @@ import Posts from "./Posts";
 
 const Feed = () => {
     return(
-        <main>
-            <section>
+        <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+            <section className="md:col-span-2">
                 <Stories />
                 <Posts />
             </section>
